cad3fe1c51f57764dfe61db1042cb3bdb2869c4d
Erika-U <flower.errr@gmail.com>
2019-07-29 22:23:31 +0900

    Edit Todo Model

--- server/app/Todo.php
+++ server/app/Todo.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    //
+  protected $table = 'todos';  //モデルとテーブルを紐づけ
+  protected $primaryKey = 'id';  //プライマリーキーをidに紐づけ
+  public $timestamps = false;  //タイムスタンプを無効にする
 }
