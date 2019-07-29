cdd93ee302c90d3720fd9571995e6933ed131778
Erika-U <flower.errr@gmail.com>
2019-07-29 22:28:39 +0900

    add getTodos at TodoController

--- server/app/Http/Controllers/TodoController.php
+++ server/app/Http/Controllers/TodoController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Todo;
 
 class TodoController extends Controller
 {
-    //
+    public function getTodos() {
+      $todos = Todo::all();
+      return $todos;
+    }
 }
