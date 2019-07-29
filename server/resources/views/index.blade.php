734a50111b8e2e6678a1edabf2ea2899f3ffe207
Erika-U <flower.errr@gmail.com>
2019-07-29 22:18:21 +0900

    create blade,sass file

---
+++ server/resources/views/index.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>TODO</title>
+     <meta name="csrf-token" content="{{ csrf_token() }}">
+     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  </head>
+  <body>
+    <div id="app">
+      <h1>ゆる〜くTODO</h1>
+      <input type="text" placeholder="すること書いてね">
+      <button type="button" class="task_add">ついか</button>
+      <table>
+        <thead>
+          <tr>
+            <th>No.</th>
+            <th>すること</th>
+            <th>おわったら押す</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>お料理</td>
+            <td><button type="button" name="button">できた</button></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <script src="{{ asset('js/app.js') }}"></script>
+  </body>
+</html>
